cb846f774914548ed567a3459f4b69c4f71ce35b
Moros Smith <moros1138@gmail.com>
2025-12-05 06:39:06 -0500

    use raylib
    use raylib

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ provides functionality on par with that provided by the Jellyfin Web client.
 
 3. Generate CMake Configuration/Build files.
    ```sh
-   cmake -S . -B build
+   cmake -S . -B build -DPLATFORM=DRM
    ```
 
 4. Build the client.
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,96 @@
 #include <mpv/client.h>
+#include "raylib.h"
+#include "rlgl.h"
 
-int main(int argc, char* argv[])
+#include <iostream>
+#include <stdlib.h>
+
+//------------------------------------------------------------------------------------
+// Global Varialbes
+//------------------------------------------------------------------------------------
+const int screenWidth = 1280;
+const int screenHeight = 720;
+
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// Update
+//------------------------------------------------------------------------------------
+void update()
 {
-    if(argc != 2)
-        return -1;
+    // Update
+    //----------------------------------------------------------------------------------
 
-    mpv_handle *ctx = mpv_create();
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+        // clear canvas to color
+        ClearBackground(PURPLE);
+        
+        // draw yellow border
+        DrawRectangleLinesEx({0.5f, -0.5f, screenWidth, screenHeight + 0.5f}, 1.0f, YELLOW);
+        
+        // draw fps
+        DrawFPS(5, 5);
+
+    EndDrawing();
     
-    mpv_initialize(ctx);
+    //----------------------------------------------------------------------------------
+}
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib - PGEtinker Classic Example");
     
-    const char *cmd[] = {"loadfile", argv[1], NULL};
-    
-    mpv_command(ctx, cmd);
-    while(true)
+#if(__EMSCRIPTEN__)
+    // Set our game to run at 60 frames-per-second
+    emscripten_set_main_loop(update, 60, 1);
+#else
+    SetTargetFPS(0);
+
+    // Main game loop
+    while(!WindowShouldClose())
     {
-        mpv_event *event = mpv_wait_event(ctx, -1);
-        if (event->event_id == MPV_EVENT_SHUTDOWN)
-            break;
+        update();
     }
+#endif
+    
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
-    mpv_terminate_destroy(ctx);
     return 0;
 }
+
+// int main(int argc, char* argv[])
+// {
+//     if(argc != 2)
+//         return -1;
+
+//     mpv_handle *ctx = mpv_create();
+    
+//     mpv_initialize(ctx);
+    
+//     const char *cmd[] = {"loadfile", argv[1], NULL};
+    
+//     mpv_command(ctx, cmd);
+//     while(true)
+//     {
+//         mpv_event *event = mpv_wait_event(ctx, -1);
+//         if (event->event_id == MPV_EVENT_SHUTDOWN)
+//             break;
+//     }
+
+//     mpv_terminate_destroy(ctx);
+//     return 0;
+// }
+
