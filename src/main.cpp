8ee0827c0c56989c65beb93224e837f5f4b7a5c4
Moros Smith <moros1138@gmail.com>
2025-12-06 20:25:58 -0500

    use SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,52 @@
-#include <mpv/client.h>
-#include "raylib.h"
-#include "rlgl.h"
+// g++ main.cpp -o sdl_drm_app `sdl2-config --cflags --libs` -lGLESv2
 
+#include <SDL2/SDL.h>
+#include <GLES2/gl2.h>
 #include <iostream>
-#include <stdlib.h>
 
-//------------------------------------------------------------------------------------
-// Global Varialbes
-//------------------------------------------------------------------------------------
-const int screenWidth = 1280;
-const int screenHeight = 720;
-
-//------------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------------
-// Update
-//------------------------------------------------------------------------------------
-void update()
+int main(int argc, char* argv[])
 {
-    // Update
-    //----------------------------------------------------------------------------------
-
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-        // clear canvas to color
-        ClearBackground(PURPLE);
-        
-        // draw yellow border
-        DrawRectangleLinesEx({0.5f, -0.5f, screenWidth, screenHeight + 0.5f}, 1.0f, YELLOW);
-        
-        // draw fps
-        DrawFPS(5, 5);
-
-    EndDrawing();
-    
-    //----------------------------------------------------------------------------------
-}
-//------------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "raylib - PGEtinker Classic Example");
-    
-#if(__EMSCRIPTEN__)
-    // Set our game to run at 60 frames-per-second
-    emscripten_set_main_loop(update, 60, 1);
-#else
-    SetTargetFPS(0);
-
-    // Main game loop
-    while(!WindowShouldClose())
-    {
-        update();
+    SDL_SetHint(SDL_HINT_VIDEODRIVER, "kmsdrm");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
     }
-#endif
-    
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 DRM/KMS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (!context) {
+        std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.4f, 0.8f, 1.0f);
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
-
-// int main(int argc, char* argv[])
-// {
-//     if(argc != 2)
-//         return -1;
-
-//     mpv_handle *ctx = mpv_create();
-    
-//     mpv_initialize(ctx);
-    
-//     const char *cmd[] = {"loadfile", argv[1], NULL};
-    
-//     mpv_command(ctx, cmd);
-//     while(true)
-//     {
-//         mpv_event *event = mpv_wait_event(ctx, -1);
-//         if (event->event_id == MPV_EVENT_SHUTDOWN)
-//             break;
-//     }
-
-//     mpv_terminate_destroy(ctx);
-//     return 0;
-// }
-
